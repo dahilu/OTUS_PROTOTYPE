45824c76bc4f6aa23b1436ab1c3c55672502a14f
DanilSafonov <dahilu@gmail.com>
2025-01-25 21:41:06 +0300

    • Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OTUS_PROTOTYPE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
